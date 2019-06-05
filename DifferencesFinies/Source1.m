4ad5a81912637eaaa0d7b4ee969ffd9f00c8461f
unknown <thomas.guily1998@gmail.com>
2019-06-05 20:29:46 +0200

    corrected n=201 bug

--- DifferencesFinies/Source1.m
+++ DifferencesFinies/Source1.m
@@ -3,10 +3,10 @@ function S = Source(z,t)
     S = zeros(length(z),1);
     
     if t <= 2
-      S(floor(length(z)/2)+1)=0.15;
+      S(length(z)/2)=0.15;
     endif
     if t > 2
-      S(floor(length(z)/2)+1) = 0;
+      S(length(z)/2) = 0;
     endif
 
 end
